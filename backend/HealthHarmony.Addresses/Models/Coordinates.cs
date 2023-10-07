12031bf3f6293171d3e6c0ca8b392b074f168fdc
PiotrSzczachor <piotr.szczachor1@wp.pl>
2023-10-08 01:43:27 +0200

    Integrate geoapify with the project

---
+++ backend/HealthHarmony.Addresses/Models/Coordinates.cs
@@ -0,0 +1,8 @@
+﻿namespace HealthHarmony.Addresses.Models
+{
+    public class Coordinates
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
