34164c10c40c9356d0f5745d97657b7d5563f5e5
PiotrSzczachor <piotr.szczachor1@wp.pl>
2023-10-25 16:16:19 +0200

    Filters UI core

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -3,15 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { ErrorInterceptor } from '../interceptors/error.interceptor';
-import { TokenInterceptor } from '../interceptors/token.interceptor';
+import { TranslateModule } from '@ngx-translate/core';
 import { IsLoggedInDirective } from '../directives/is-logged-in.directive';
 import { IsNotLoggedInDirective } from '../directives/is-not-logged-in.directive';
 import { DecodePipe } from '../pipes/decode.pipe';
 import { PaginatorComponent } from './paginator/paginator.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
 
 const DIRECTIVES = [
     IsLoggedInDirective, IsNotLoggedInDirective
@@ -32,17 +31,23 @@ const COMPONENTS = [
     MatIconModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    TranslateModule
+    TranslateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   exports: [
     MatIconModule,
     BrowserAnimationsModule,
     MatSidenavModule,
     TranslateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
     DIRECTIVES,
     PIPES,
     COMPONENTS
-  ]
+  ],
 })
 
 export class SharedModule { }
